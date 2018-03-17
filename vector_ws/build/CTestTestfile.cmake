ce39649d9020518ddc4c539f45c14c17c5b20af5
Unknown <likewatchk@gmail.com>
2018-03-18 02:35:20 +0900

    vector_ws

---
+++ vector_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/softkoo/vector_ws/src
+# Build directory: /home/softkoo/vector_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(curb_test_pkg)
---
+++ vector_ws/build/curb_test_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/softkoo/vector_ws/src/curb_test_pkg
+# Build directory: /home/softkoo/vector_ws/build/curb_test_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
